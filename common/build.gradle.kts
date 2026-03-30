cdde4b1bef475771b29351448ce2bfd9e92db7ff
Lukas Jost <lukas.jost@outlook.com>
2026-03-30 19:42:48 +0200

    chore(deps): update ktor and junit (#23)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,9 +3,9 @@ plugins { `java-library` }
 dependencies {
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     api("com.google.code.gson:gson:2.11.0")
-    implementation("io.ktor:ktor-client-core:3.3.3")
-    implementation("io.ktor:ktor-client-cio:3.3.3")
+    implementation("io.ktor:ktor-client-core:3.4.2")
+    implementation("io.ktor:ktor-client-cio:3.4.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:6.0.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.0.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
