932d12b2bce9b86a96375d91bbdcb48c25cd903a
Lukas Jost <lukas.jost@outlook.com>
2026-06-16 03:47:09 +0200

    chore: update minestom runtime dependency

--- minestom/build.gradle.kts
+++ minestom/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("gg.grounds.minestom-conventions") }
 
 dependencies {
-    api("gg.grounds:grounds-minestom-runtime-runtime-api:0.1.0")
+    api("gg.grounds:grounds-minestom-runtime-runtime-api:0.2.0")
     implementation(project(":common"))
     implementation("org.slf4j:slf4j-api:2.0.18")
 
