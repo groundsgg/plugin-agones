ae4603a77db183ab8f852bf60405576496be0e09
Lukas Jost <lukas.jost@outlook.com>
2026-06-17 03:06:01 +0200

    fix: use released minestom runtime api

--- minestom/src/main/kotlin/gg/grounds/GroundsPluginAgonesProvider.kt
+++ minestom/src/main/kotlin/gg/grounds/GroundsPluginAgonesProvider.kt
@@ -2,10 +2,12 @@ package gg.grounds
 
 import gg.grounds.runtime.GroundsModule
 import gg.grounds.runtime.GroundsModuleProvider
+import gg.grounds.runtime.ServerType
 
 class GroundsPluginAgonesProvider : GroundsModuleProvider {
     override val id: String = GroundsPluginAgones.MODULE_ID
     override val version: String = GroundsPluginAgones.VERSION
+    override val serverTypes: Set<ServerType> = ServerType.entries.toSet()
 
     override fun create(): GroundsModule = GroundsPluginAgones()
 }
