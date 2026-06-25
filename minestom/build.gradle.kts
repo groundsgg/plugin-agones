7ad97f8d312366f65555fee55a81ae85056b26a1
Lukas Jost <lukas.jost@outlook.com>
2026-06-25 16:46:15 +0200

    feat: use grounds dependencies bom (#56)

--- minestom/build.gradle.kts
+++ minestom/build.gradle.kts
@@ -11,10 +11,12 @@ buildConfig {
 }
 
 dependencies {
+    implementation(platform("gg.grounds:grounds-dependencies:0.1.0"))
+
     api("gg.grounds:grounds-minestom-runtime-runtime-api:0.1.0")
     implementation(project(":common"))
-    implementation("org.slf4j:slf4j-api:2.0.18")
+    implementation("org.slf4j:slf4j-api")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:6.1.0")
+    testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
